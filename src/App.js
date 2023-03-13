bf6538bcddcfb1dceaec27c6b33fa9edb44139f2
abenaTenkorang <abenagy16@gmail.com>
2023-03-13 20:00:02 +0000

    loop through notes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Note from './components/Note';
+import notes from './notes';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <Note
-        title="This is the title"
-        content="This is the content"
-      />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
