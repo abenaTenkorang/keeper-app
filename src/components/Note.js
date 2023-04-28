0c688c788d95de71b8a268c1cb40ac18690c2af8
abenaTenkorang <abenagy16@gmail.com>
2023-04-28 12:07:09 +0000

    Add a delete button to notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
-function Note({ title, content }) {
+function Note({
+  id, title, content, handleDelete,
+}) {
+  const handleClick = () => {
+    handleDelete(id);
+  };
   return (
     <div className="note">
       <h1>{title}</h1>
       <p>{content}</p>
+      <button type="button" onClick={handleClick}>DELETE</button>
     </div>
   );
 }
